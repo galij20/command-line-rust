63f39d891aee321233fdeab9a6e8884892ce20e1
Galij Sunuwar <galijsunuwar920@gmail.com>
2025-07-28 22:54:06 +0545

    Configure help section

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Galij Sunuwar <galijsunuwar920@gmail.com>")
+        .about("Rust echo")
+        .get_matches();
 }
