1b7929b20785f302538f645285d97523f881fdd0
Galij Sunuwar <galijsunuwar920@gmail.com>
2025-07-29 14:37:40 +0545

    Add echo functionality

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -19,6 +19,10 @@ fn main() {
             .takes_value(false),
         )
         .get_matches();
+        
+    let text = matches.values_of_lossy("text").unwrap();
 
-    println!("{:#?}", matches);
+    let omit_newline = matches.is_present("omit_newline");
+
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" });
 }
